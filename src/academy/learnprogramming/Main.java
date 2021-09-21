9eea198bc43a1414c8f73215b5cdb57c282ccdcd
Luke MacLean <40969439+MacLeanLuke@users.noreply.github.com>
2021-09-20 23:23:49 -0500

    Initial commit

---
+++ src/academy/learnprogramming/Main.java
@@ -0,0 +1,21 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        int myVariable = 50;
+        myVariable++;
+        myVariable--;
+        System.out.println("This is a test");
+
+        System.out.println("This is " + // indenting issues are visual but the code will still run
+                " another " +
+                " still more.");
+        int anotherVariable = 50;
+        myVariable--;
+        System.out.println("This is another one"); // whitepace issues make code harder to read, but Java will still read it.
+
+
+    }
+}
